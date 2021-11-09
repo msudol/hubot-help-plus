f0a38b119929e01c79f9f669fb0823b3da847050
msudol <matt.sudol@gmail.com>
2021-11-08 20:51:48 -0500

    the original help plugin with a few of my modifications already in place.. lots of work to do

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # hubot-help-plus
-An upgrade to the original hubot-help. Show available hubot commands with improvements.
+
+Show available hubot commands with improvements. An upgrade to the original hubot-help https://github.com/hubotio/hubot-help. 
+
+Note: If you are using this hubot script, remove hubot-help as the commands will conflict.
+
+## Installation
+
+Not yet published on NPM, still in development. Coming soon.
+
+## Configuration
+
+You can set various environment variables to tune up the behavior of thios help plugin:
+
+- `HUBOT_HELP_REPLY_IN_PRIVATE` (set to any value) will force calls to `hubot help` to be answered in private
+- `HUBOT_HELP_DISABLE_HTTP` (set to any value) will disable the web interface for help
+- `HUBOT_HELP_HIDDEN_COMMANDS` comma-separated list of commands that will not be displayed in help
+
+## Improvements Upon Original
+
+- Fixed webpage display
+- Fixed several errors and bugs
+- Fixing 2000 character limit response for adapters like slack and discord
+- Fixing hidden commands env variable not working
+
+
---
+++ src/help-plus.js
@@ -0,0 +1,168 @@
+'use strict'
+
+// Description:
+//   Generates help commands for Hubot.
+//
+// Commands:
+//   hubot help - Displays all of the help commands that this bot knows about.
+//   hubot help <query> - Displays all help commands that match <query>.
+//
+// URLS:
+//   /hubot/help
+//
+// Configuration:
+//   HUBOT_HELP_REPLY_IN_PRIVATE - if set to any value, all `hubot help` replies are sent in private
+//   HUBOT_HELP_DISABLE_HTTP - if set, no web entry point will be declared
+//   HUBOT_HELP_HIDDEN_COMMANDS - comma-separated list of commands that will not be displayed in help
+//
+// Notes:
+//   These commands are grabbed from comment blocks at the top of each file.
+// 
+// Author:
+//   Matt Sudol
+//   Based on the original hubot-help
+
+
+//TODO: ugly formatting need to change this eventually
+const helpContents = (name, commands) => `\
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title>${name} help</title>
+  <style type="text/css">
+    body {
+      background: #ddd;
+      color: #000;
+      text-shadow: 0 1px 1px rgba(255, 255, 255, .5);
+    }
+    h1 {
+      margin: 8px 0;
+      padding: 0;
+    }
+    .commands {
+      font-size: 14px;
+    }
+    p {
+      border-bottom: 1px solid #eee;
+      margin: 6px 0 0 0;
+      padding-bottom: 5px;
+    }
+    p:last-child {
+      border: 0;
+    }
+  </style>
+  </head>
+  <body>
+    <h1>${name} Help</h1>
+    <div class="commands">
+      ${commands}
+    </div>
+  </body>
+</html>\
+`
+
+// routine to get hiden commands
+var hiddenCommandsPattern = function() {
+  var hiddenCommands, ref;
+  hiddenCommands = (ref = process.env.HUBOT_HELP_HIDDEN_COMMANDS) != null ? ref.split(',') : void 0;
+  if (hiddenCommands) {
+    return new RegExp("^hubot (?:" + (hiddenCommands != null ? hiddenCommands.join('|') : void 0) + ") - ");
+  }
+};
+
+// routine to parse the help commands
+var getHelpCommands = function getHelpCommands (robot) {
+  
+    // pulling the helpCommands from the robot
+    let helpCommands = robot.helpCommands();
+    const robotName = robot.alias || robot.name;
+
+    // filter
+    if (hiddenCommandsPattern()) {
+        helpCommands = helpCommands.filter(command => !hiddenCommandsPattern().test(command));
+    }
+
+    helpCommands = helpCommands.map((command) => {
+        if (robotName.length === 1) {
+            return command.replace(/^hubot\s*/i, robotName);
+        }
+
+        return command.replace(/^hubot/i, robotName);
+    })
+
+    return helpCommands.sort();
+}
+
+// main function for the script
+module.exports = (robot) => {
+    
+  // if reply in private will be set
+  const replyInPrivate = process.env.HUBOT_HELP_REPLY_IN_PRIVATE;
+  
+  // listen for "hubot help <query>"
+  robot.respond(/help(?:\s+(.*))?$/i, (msg) => {
+    
+    // get the command list
+    let cmds = getHelpCommands(robot);
+    const filter = msg.match[1];
+
+    if (filter) {
+      cmds = cmds.filter(cmd => cmd.match(new RegExp(filter, 'i')))
+      if (cmds.length === 0) {
+        msg.send("No available commands match" + filter);
+        return;
+      }
+    }
+
+    let emit = cmds.join('\n')
+    
+    // temporary if the help is too long just don't let it print
+    if (emit.length >=2000) {
+
+        //trim the string to the maximum length
+        var trimmedString = emit.substr(0, 1900);
+
+        //re-trim if we are in the middle of a word
+        trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf("\n")))        
+        
+        emit = trimmedString + "\n... help too long, cutting display short";
+    }
+    
+    // reply in private, check adapter for special cases like slack and discord
+    if (replyInPrivate && (robot.adapterName === 'slack' || robot.adapterName === 'discord' || robot.adapterName === 'discobot') && msg.message && msg.message.user && msg.message.user.id) {
+      msg.reply('replied to you in private!')
+      return robot.send({ room: msg.message.user.id }, emit)
+    } 
+    else if (replyInPrivate && msg.message && msg.message.user && msg.message.user.name) {
+      msg.reply('replied to you in private!');
+      return robot.send({ room: msg.message.user.name }, emit);
+    } 
+    else {
+      return msg.send(emit);
+    }
+  })
+
+  // display the help on the bot webpage
+  if (process.env.HUBOT_HELP_DISABLE_HTTP == null) {
+    
+    // setup web url  
+    return robot.router.get("/" + robot.name + "/help", (req, res) => {
+        
+      // get the available commands
+      let cmds = getHelpCommands(robot).map(cmd => cmd.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'))
+
+      if (req.query.q != null) {
+        cmds = cmds.filter(cmd => cmd.match(new RegExp(req.query.q, 'i')))
+      }
+
+      let emit = `<p>${cmds.join('</p><p>')}</p>`
+
+      emit = emit.replace(new RegExp(`${robot.name}`, 'ig'), `<b>${robot.name}</b>`)
+
+      res.setHeader('content-type', 'text/html')
+      res.end(helpContents(robot.name, emit))
+    })
+  }
+  
+}
